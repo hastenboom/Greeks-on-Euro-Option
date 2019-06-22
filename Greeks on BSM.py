11b8c36d11be825206f5d1ea7632e5f6f16287ee
hastenboom <42961312+hastenboom@users.noreply.github.com>
2019-06-23 00:31:38 +0800

    Update Greeks on BSM.py

--- Greeks on BSM.py
+++ Greeks on BSM.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats as ss
 from scipy.stats import norm as n
+from plotnine import *
 
 s, K, sigma, r, inte, option = np.linspace(0.00001, 100, 100), 50, 0.28, 0.05, 9 / 12, "p"
 
@@ -12,13 +13,10 @@ class Greeks:
         # S price of underlying price
         # K the strike price # sigma the volatility, implied volatility
         # r risk free rate # inte time interval
-        self.s = s
-        self.K = K
-        self.sigma = sigma
-        self.r = r
-        self.inte = inte
-        self.option = option
-        self.d1 = (np.log(self.s / self.K) + (self.r + self.sigma ** 2 / 2) * self.inte) / (self.sigma * np.sqrt(self.inte))
+        self.s, self.K, self.sigma, self.r = s, K, sigma, r
+        self.inte, self.option = inte, option
+        self.d1 = (np.log(self.s / self.K) + (self.r + self.sigma ** 2 / 2) * self.inte) / (
+                self.sigma * np.sqrt(self.inte))
         self.d2 = self.d1 - self.sigma * np.sqrt(self.inte)
 
     def payoff(self):
@@ -37,11 +35,13 @@ class Greeks:
         value = np.zeros(self.s.__len__())
         if (self.option == 'c'):
             for i in range(0, (s.__len__())):
-                value[i] = self.s[i]*n.cdf(self.d1[i]) - self.K*np.exp(1)**(-self.r*self.inte)*n.cdf(self.d2[i])
+                value[i] = self.s[i] * n.cdf(self.d1[i]) - self.K * np.exp(1) ** (-self.r * self.inte) * n.cdf(
+                    self.d2[i])
 
         if (self.option == 'p'):
             for i in range(0, (self.s.__len__())):
-                value[i] = self.K*np.exp(1) ** (-self.r * self.inte) * n.cdf(-1*self.d2[i]) - self.s[i] * n.cdf(-1*self.d1[i])
+                value[i] = self.K * np.exp(1) ** (-self.r * self.inte) * n.cdf(-1 * self.d2[i]) - self.s[i] * n.cdf(
+                    -1 * self.d1[i])
 
         return value
 
@@ -51,31 +51,62 @@ class Greeks:
             for i in range(0, (self.s.__len__())):
                 delta[i] = n.cdf(self.d1[i])
 
-        if(self.option == 'p'):
+        if (self.option == 'p'):
             for i in range(0, (s.__len__())):
-                delta[i] = n.cdf(self.d1[i])-1
+                delta[i] = n.cdf(self.d1[i]) - 1
         return delta
 
     def gamma(self):
         gamma = np.zeros(self.s.__len__())
         for i in range(0, (self.s.__len__())):
-            gamma[i] = (np.exp(1)**(-0.5*self.d1[i]**2)) / (self.s[i]*self.sigma*np.sqrt(2*np.pi*self.inte))
+            gamma[i] = (np.exp(1) ** (-0.5 * self.d1[i] ** 2)) / (
+                    self.s[i] * self.sigma * np.sqrt(2 * np.pi * self.inte))
 
         return gamma
 
     def vega(self):
         vega = np.zeros(self.s.__len__())
         for i in range(0, (self.s.__len__())):
-            vega[i] = self.s[i] * np.sqrt(inte) * (np.exp(1)**(-self.d1[i]**2/2)) / np.sqrt(2*np.pi)
+            vega[i] = self.s[i] * np.sqrt(inte) * (np.exp(1) ** (-self.d1[i] ** 2 / 2)) / np.sqrt(2 * np.pi)
 
         return vega
 
 
+call = Greeks(s, K, sigma, r, inte, 'c')
+put = Greeks(s, K, sigma, r, inte, 'p')
 
-x1 = Greeks(s, K, sigma, r, inte, 'c')
+# value and payoff
+value = pd.DataFrame({'price': call.s,
+                      'value_c': call.value(),
+                      'value_p': put.value(),
+                      'payoff_c': call.payoff(),
+                      'payoff_p': put.payoff()})
+value_melt = value.melt(id_vars='price')
 
-x1.value()
-plt.plot(s,x1.value())
-plt.plot(s,x1.payoff())
-plt.plot(s,x1.delta())
-plt.plot(s,x1.vega())
+value_melt['a'] = np.nan
+value_melt.a[value_melt.variable.str.contains('_p')] = 'put'
+value_melt.a[~value_melt.variable.str.contains('_p')] = 'call'
+
+(ggplot(data=value_melt, mapping=aes('price', 'value', color='variable')) +
+ geom_line(size=1.5) +
+ facet_wrap('~a', nrow=2, ncol=1) +
+ scale_color_manual(['Steelblue', 'Steelblue', 'black', 'black']))
+
+# the Greeks
+greeks = pd.DataFrame({'price': call.s, 'delta_c': call.delta(),
+                       'delta_p': put.delta(), 'gamma_c': call.gamma(),
+                       'gamma_p': put.gamma(), 'vega_c': call.vega(),
+                       'vega_p': put.vega()})
+
+greeks_melt = greeks.melt(id_vars='price')
+
+greeks_melt['type'] = np.nan
+greeks_melt.type[greeks_melt.variable.str.contains('_p')] = 'put'
+greeks_melt.type[greeks_melt.variable.str.contains('_c')] = 'call'
+greeks_melt.variable=greeks_melt.variable.str.replace('_c',' ')
+greeks_melt.variable=greeks_melt.variable.str.replace('_p',' ')
+
+(ggplot(greeks_melt,aes('price','value',color='type'))+
+ geom_line(size=1.5)+
+ facet_wrap('~variable',nrow=3,ncol=2,shrink = 'FALSE'))
+
